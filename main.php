a9602de4050a9fdf676a3c2729f391e33d2f9d52
Marai de Jong <yaidenmarai@gmail.com>
2024-02-26 15:47:10 +0100

    main veranderd naar een grid

--- main.php
+++ main.php
@@ -12,7 +12,7 @@
 
 <body>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<!-- <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,7 +26,71 @@
     </div>
   </div>
 </nav>
-<a href="thema.php">Beemster kwartet</a>
+<a href="thema.php">Beemster kwartet</a> -->
+
+
+
+<style>
+body {
+  font-family: arial;
+  font-size: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+
+.wrapper {
+  position: relative;
+  flex-grow: 1;
+  margin: auto;
+  max-width: 1200px;
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+  grid-gap: 15px;
+  justify-content: center;
+  align-items: center;
+}
+
+.wrapper img {
+  z-index: 1;
+  grid-column: span 2;
+  max-width: 100%;
+  margin-bottom: -52%;
+  clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
+  transform: scale(1);
+  transition: all 0.25s;
+}
+
+.wrapper img:nth-child(7n+1) {
+  grid-column: 2/span 2;
+}
+
+.wrapper img:hover {
+  z-index: 2;
+  transform: scale(2);
+}
+
+</style>
+
+<div class="wrapper">
+  <img src="images/Vlakje_groen.svg" alt="...">
+  <img src="images/Vlakje_groen.svg" alt="...">
+  <img src="images/Vlakje_groen.svg" alt="...">
+  <img src="images/Vlakje_geel.svg" alt="...">
+  <img src="images/Vlakje_geel.svg" alt="...">
+  <img src="images/Vlakje_geel.svg" alt="...">
+  <img src="images/Vlakje_rood.svg" alt="...">
+  <img src="images/Vlakje_rood.svg" alt="...">
+  <img src="images/Vlakje_rood.svg" alt="...">
+  <img src="images/Vlakje_blauw.svg" alt="...">
+  <img src="images/Vlakje_blauw.svg" alt="...">
+  <img src="images/Vlakje_blauw.svg" alt="...">
+  <img src="images/Vlakje_blauw.svg" alt="...">
+</div>
+
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
