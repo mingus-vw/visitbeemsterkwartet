c6c6fc93bfb1409617e429de6012b8c44010e54a
Marai <163207@student.horizoncollege.nl>
2024-03-01 11:25:44 +0100

    navbar weg + hoofdletters weg

--- main.php
+++ main.php
@@ -14,25 +14,7 @@
 </head>
 
 <body>
-
-    <!-- <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-    </div>
-  </div>
-</nav>
-<a href="thema.php">Beemster kwartet</a> -->
-
-
-
+    
     <style>
     body {
         font-family: arial;
@@ -72,23 +54,23 @@
     </style>
 
     <div class="wrapper">
-        <img src="images/Vlakje_groen.svg" alt="...">
-        <img src="images/Vlakje_groen.svg" alt="...">
-        <img src="images/Vlakje_groen.svg" alt="...">
-        <img src="images/Vlakje_geel.svg" alt="...">
-        <img src="images/Vlakje_geel.svg" alt="...">
-        <img src="images/Vlakje_geel.svg" alt="...">
-        <img src="images/Vlakje_geel.svg" alt="...">
-        <img src="images/Vlakje_rood.svg" alt="...">
-        <img src="images/Vlakje_rood.svg" alt="...">
-        <img src="images/Vlakje_rood.svg" alt="...">
-        <img src="images/Vlakje_blauw.svg" alt="...">
-        <img src="images/Vlakje_blauw.svg" alt="...">
-        <img src="images/Vlakje_blauw.svg" alt="...">
-        <img src="images/Vlakje_blauw.svg" alt="...">
-        <img src="images/Vlakje_blauw.svg" alt="...">
-        <img src="images/Vlakje_blauw.svg" alt="...">
-        <img src="images/Vlakje_blauw.svg" alt="...">
+        <img src="images/vlakje_groen.svg" alt="...">
+        <img src="images/vlakje_groen.svg" alt="...">
+        <img src="images/vlakje_groen.svg" alt="...">
+        <img src="images/vlakje_geel.svg" alt="...">
+        <img src="images/vlakje_geel.svg" alt="...">
+        <img src="images/vlakje_geel.svg" alt="...">
+        <img src="images/vlakje_geel.svg" alt="...">
+        <img src="images/vlakje_rood.svg" alt="...">
+        <img src="images/vlakje_rood.svg" alt="...">
+        <img src="images/vlakje_rood.svg" alt="...">
+        <img src="images/vlakje_blauw.svg" alt="...">
+        <img src="images/vlakje_blauw.svg" alt="...">
+        <img src="images/vlakje_blauw.svg" alt="...">
+        <img src="images/vlakje_blauw.svg" alt="...">
+        <img src="images/vlakje_blauw.svg" alt="...">
+        <img src="images/vlakje_blauw.svg" alt="...">
+        <img src="images/vlakje_blauw.svg" alt="...">
     </div>
 
 
