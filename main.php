44b38db5ca9f56dfa7bb91728dc839bbbee52490
Mingus Wankum <16616@student.horizoncollege.nl>
2024-03-01 11:42:15 +0100

    ijijij

--- main.php
+++ main.php
@@ -14,7 +14,7 @@
 </head>
 
 <body>
-    
+
     <style>
     body {
         font-family: arial;
@@ -54,30 +54,18 @@
     </style>
 
     <div class="wrapper">
-        <img src="images/vlakje_groen.svg" alt="...">
-        <img src="images/vlakje_groen.svg" alt="...">
-        <img src="images/vlakje_groen.svg" alt="...">
-        <img src="images/vlakje_geel.svg" alt="...">
-        <img src="images/vlakje_geel.svg" alt="...">
-        <img src="images/vlakje_geel.svg" alt="...">
-        <img src="images/vlakje_geel.svg" alt="...">
+        <img src="images/half_rood_links.svg" alt="...">
         <img src="images/vlakje_rood.svg" alt="...">
-        <img src="images/vlakje_rood.svg" alt="...">
-        <img src="images/vlakje_rood.svg" alt="...">
-        <img src="images/vlakje_blauw.svg" alt="...">
-        <img src="images/vlakje_blauw.svg" alt="...">
-        <img src="images/vlakje_blauw.svg" alt="...">
-        <img src="images/vlakje_blauw.svg" alt="...">
-        <img src="images/vlakje_blauw.svg" alt="...">
-        <img src="images/vlakje_blauw.svg" alt="...">
-        <img src="images/vlakje_blauw.svg" alt="...">
-    </div>
+        <img src="images/half_rood_rechts.svg" alt="...">
+
+     </div>
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+        </script>
 </body>
 
 </html>
