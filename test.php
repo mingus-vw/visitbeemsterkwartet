c5cf268f917215fe749740e018a311f05bbd5257
Marai de Jong <yaidenmarai@gmail.com>
2024-03-05 14:08:08 +0100

    test

--- test.php
+++ test.php
@@ -4,117 +4,121 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
-        body {
-            margin: 0;
-            padding: 0;
-        }
+        .main {
+  display:flex;
+  --s: 100px;  /* size  */
+  --m: 4px;    /* space */
+  --r: calc(var(--s) + 4*var(--m) - 2px);
+}
 
-        ul {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            margin: 0;
-            padding: 0;
-            width: 600px;
-            height: 600px;
-        }
+.container {
+  font-size: 0; /*disable white space between inline block element */
+}
 
-        ul li {
-            list-style: none;
-            position: absolute;
-            width: 200px;
-            height: 200px;
-            background: #000;
-            transform: rotate(45deg);
-            transition: .5s;
-            margin: -100px;
-        }
-
-        ul li.item1 {
-            top: 0;
-            left: 0;
-        }
-
-        ul li.item2 {
-            top: 0;
-            left: 50%;
-        }
-
-        ul li.item3 {
-            top: 0;
-            left: 100%;
-        }
-
-        ul li.item4 {
-            top: 50%;
-            left: 0;
-        }
-
-        ul li.item5 {
-            top: 50%;
-            left: 50%;
-        }
-
-        ul li.item6 {
-            top: 50%;
-            left: 100%;
-        }
-
-        ul li.item7 {
-            top: 100%;
-            left: 0;
-        }
-
-        ul li.item8 {
-            top: 100%;
-            left: 50%;
-        }
-
-        ul li.item9 {
-            top: 100%;
-            left: 100%;
-        }
-
-        /* Additional rhombuses for the gaps */
-        ul li.item10 {
-            top: 25%;
-            left: 25%;
-        }
-
-        ul li.item11 {
-            top: 25%;
-            left: 75%;
-        }
-
-        ul li.item12 {
-            top: 75%;
-            left: 25%;
-        }
-
-        ul li.item13 {
-            top: 75%;
-            left: 75%;
-        }
+.container div {
+  width: var(--s);
+  margin: var(--m);
+  height: var(--s);
+  display: inline-block;
+  font-size:initial;
+  clip-path: polygon(50% 0, 100% 50%, 50% 100%, 0 50%);
+  background: red;
+  margin-bottom: calc(var(--m) - var(--s)*0.5);
+}
+.container div:nth-child(odd) {
+  background:green;
+}
+.container::before {
+  content: "";
+  width: calc(var(--s)/2 + var(--m));
+  float: left;
+  height: 140%;
+  shape-outside: repeating-linear-gradient(     
+                   #0000 0 calc(var(--r) - 3px),      
+                   #000  0 var(--r));
+} 
     </style>
 </head>
 <body>
-    <ul>
-        <li class="item1"><div class="bg"></div></li>
-        <li class="item2"><div class="bg"></div></li>
-        <li class="item3"><div class="bg"></div></li>
-        <li class="item4"><div class="bg"></div></li>
-        <li class="item5"><div class="bg"></div></li>
-        <li class="item6"><div class="bg"></div></li>
-        <li class="item7"><div class="bg"></div></li>
-        <li class="item8"><div class="bg"></div></li>
-        <li class="item9"><div class="bg"></div></li>
-        
-        <!-- Additional rhombuses for the gaps -->
-        <li class="item10"><div class="bg"></div></li>
-        <li class="item11"><div class="bg"></div></li>
-        <li class="item12"><div class="bg"></div></li>
-        <li class="item13"><div class="bg"></div></li>
-    </ul>
+
 </body>
 </html>
+
+
+
+<div class="main">
+  <div class="container">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</div>
