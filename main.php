8b9b5fb77431d139516b6e21271b463be25de7c4
Mingus Wankum <16616@student.horizoncollege.nl>
2024-03-08 12:39:11 +0100

    eee

--- main.php
+++ main.php
@@ -64,11 +64,7 @@
         <img src="images/vlakje_rood.svg" alt="...">
         <img src="images/half_rood_rechts.svg" alt="...">
 
-     </div>
-
-
-
-
+    </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
         </script>
