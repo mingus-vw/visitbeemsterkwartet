c9e6fc3319e67e5b1eb9b1b17acb06cd68ac159a
Marai <163207@student.horizoncollege.nl>
2024-03-19 12:05:29 +0100

    modal

--- test.php
+++ test.php
@@ -72,9 +72,20 @@
     grid-column: 7 / span 2;
     clip-path: polygon(50% 0%, 50% 50%, 50% 100%, 0% 50%);
 }
+
+.ja {
+    z-index: 1;
+    grid-column: span 2;
+    max-width: 100%;
+    margin-bottom: -52%;
+    clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
+    transform: scale(1);
+    transition: all 0.25s;
+}
     </style>
 
 <div class="wrapper">
+  <img  class="ja" src="images/kip.png" alt="...">
   <img class="blocks" src="images/vlakje_rood.svg" alt="...">
   <img class="blocks" src="images/vlakje_rood.svg" alt="...">
   <img class="blocks" src="images/vlakje_rood.svg" alt="...">
@@ -98,7 +109,6 @@
 </div>
 
 
-<!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
   Launch demo modal
 </button>
@@ -112,22 +122,75 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        
-
-      <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="images/varken.png" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Varken</h5>
-    <button class="btn btn-primary" type="button" onclick="toggleCollapse()">
-        Lees meer
-    </button>
-    <div class="collapse" id="collapseExample">
-        <div class="card-text">
-            Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="card" style="width: 16rem;">
+              <img class="card-img-top" src="images/varken.png" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Varken</h5>
+                <button class="btn btn-primary" type="button" onclick="toggleCollapse('collapseVarken')">
+                  Lees meer
+                </button>
+                <div class="collapse" id="collapseVarken">
+                  <div class="card-text">
+                    Varken placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="card" style="width: 16rem;">
+              <img class="card-img-top" src="images/koe.png" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Koe</h5>
+                <button class="btn btn-primary" type="button" onclick="toggleCollapse('collapseKoe')">
+                  Lees meer
+                </button>
+                <div class="collapse" id="collapseKoe">
+                  <div class="card-text">
+                    Koe placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
-</div>
-</div>
+
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="card" style="width: 16rem;">
+              <img class="card-img-top" src="images/schaap.png" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Schaap</h5>
+                <button class="btn btn-primary" type="button" onclick="toggleCollapse('collapseSchaap')">
+                  Lees meer
+                </button>
+                <div class="collapse" id="collapseSchaap">
+                  <div class="card-text">
+                    Schaap placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="card" style="width: 16rem;">
+              <img class="card-img-top" src="images/kip.png" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Kip</h5>
+                <button class="btn btn-primary" type="button" onclick="toggleCollapse('collapseKip')">
+                  Lees meer
+                </button>
+                <div class="collapse" id="collapseKip">
+                  <div class="card-text">
+                    Kip placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
 
       </div>
@@ -135,20 +198,19 @@
   </div>
 </div>
 
-
-
 <script>
-    function toggleCollapse() {
-        var collapseElement = document.getElementById('collapseExample');
-        var isCollapsed = collapseElement.classList.contains('show');
-        
-        if (isCollapsed) {
-            collapseElement.classList.remove('show');
-        } else {
-            collapseElement.classList.add('show');
-        }
+  function toggleCollapse(collapseId) {
+    var collapseElement = document.getElementById(collapseId);
+    var isCollapsed = collapseElement.classList.contains('show');
+    
+    if (isCollapsed) {
+      collapseElement.classList.remove('show');
+    } else {
+      collapseElement.classList.add('show');
     }
+  }
 </script>
+
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
