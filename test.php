4b424bcc7faae16d3ffcfe9c2e107ccde34ca1db
Marai <163207@student.horizoncollege.nl>
2024-03-19 12:20:43 +0100

    modal

--- test.php
+++ test.php
@@ -115,7 +115,7 @@
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-xl">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
         <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
