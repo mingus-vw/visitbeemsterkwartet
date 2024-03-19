c13e9d02476dc36e195dc5d05597c613e6ae584d
Mingus Wankum <16616@student.horizoncollege.nl>
2024-03-19 14:16:30 +0100

    update

--- main.php
+++ main.php
@@ -5,32 +5,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
         body {
-            margin: 0;
-            font-family: Arial, sans-serif;
-            overflow: hidden; /* Hide overflow to prevent scrollbars */
+            font-family: arial;
+            font-size: 16px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            min-height: 100vh;
+            overflow: hidden; /* Remove scrollbar */
+            margin: 0; /* Remove default margin */
         }
 
         .wrapper {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            display: grid;
-            grid-template-columns: repeat(auto-fill, minmax(100px, 1fr)); /* Adjust column size as needed */
-            grid-template-rows: repeat(auto-fill, minmax(100px, 1fr)); /* Adjust row size as needed */
+            position: relative;
+            flex-grow: 1;
+            margin: auto;
+            max-width: none;
+            display: inline-grid;
+            grid-template-columns: repeat(12, 1fr);
+            grid-template-rows: repeat(6, 2fr);
             grid-gap: 15px;
             justify-content: center;
             align-items: center;
-            z-index: -1; /* Move behind other content */
+            width: 100vw; /* Ensure the wrapper spans the entire viewport width */
+            height: 100vh; /* Ensure the wrapper spans the entire viewport height */
         }
 
-        .blocks {
+        .wrapper img {
             z-index: 1;
+            grid-column: span 2 / auto;
             max-width: 100%;
             margin-bottom: -52%;
             clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
@@ -38,63 +42,61 @@
             transition: all 0.25s;
         }
 
-        .wrapper img:nth-child(7n+1) {
-            grid-column: 2/span 2;
+        .wrapper img:nth-child(11n+1) {
+            grid-column: 2 / span 2;
         }
 
         .wrapper img:hover {
             z-index: 2;
             transform: scale(1.1);
         }
-
-        .half-left,
-        .half-right {
-            max-width: 100%;
-            margin-bottom: -52%;
-            clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
-            transform: scale(1);
-            transition: all 0.25s;
-        }
-
-        .half-left {
-            max-width: 100%;
-            margin-bottom: -52%;
-            clip-path: polygon(50% 0%, 50% 50%, 50% 100%, 100% 50%);
-            transform: scale(1);
-            transition: all 0.25s;
-        }
-
-        .half-right {
-            max-width: 100%;
-            margin-bottom: -52%;
-            clip-path: polygon(50% 0%, 50% 50%, 50% 100%, 0% 50%);
-            transform: scale(1);
-            transition: all 0.25s;
-        }
     </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
 </head>
 
 <body>
+
     <div class="wrapper">
-        <img class="blocks" src="images/vlakje_rood.svg" alt="...">
-        <img class="blocks" src="images/vlakje_rood.svg" alt="...">
-        <img class="blocks" src="images/vlakje_rood.svg" alt="...">
-        <img class="blocks" src="images/vlakje_groen.svg" alt="...">
-        <img class="blocks" src="images/vlakje_groen.svg" alt="...">
-        <img class="blocks" src="images/vlakje_groen.svg" alt="...">
-        <img class="blocks" src="images/vlakje_groen.svg" alt="...">
-        <img class="blocks" src="images/vlakje_groen.svg" alt="...">
-        <img class="blocks" src="images/vlakje_groen.svg" alt="...">
-        <img class="blocks" src="images/vlakje_groen.svg" alt="...">
-        <img class="blocks" src="images/vlakje_groen.svg" alt="...">
-        <img class="blocks" src="images/vlakje_geel.svg" alt="...">
-        <img class="blocks" src="images/vlakje_geel.svg" alt="...">
-        <img class="blocks" src="images/vlakje_geel.svg" alt="...">
-        <img class="blocks" src="images/vlakje_blauw.svg" alt="...">
-        <img class="blocks" src="images/vlakje_blauw.svg" alt="...">
-        <img class="blocks" src="images/vlakje_blauw.svg" alt="...">
-        <img class="blocks" src="images/vlakje_blauw.svg" alt="...">
+    <img src="images/vlakje_rood.svg" alt="rood">
+    <img src="images/vlakje_rood.svg" alt="rood">
+    <img src="images/vlakje_rood.svg" alt="rood">
+    <img src="images/vlakje_rood.svg" alt="rood">
+    <img src="images/vlakje_rood.svg" alt="rood">
+    <img src="images/vlakje_rood.svg" alt="rood">
+    <img src="images/vlakje_rood.svg" alt="rood">
+    <img src="images/vlakje_rood.svg" alt="rood">
+    <img src="images/vlakje_rood.svg" alt="rood">
+    <img src="images/vlakje_rood.svg" alt="rood">
+    <img src="images/vlakje_rood.svg" alt="rood">
+    <img src="images/vlakje_groen.svg" alt="groen">
+    <img src="images/vlakje_groen.svg" alt="groen">
+    <img src="images/vlakje_groen.svg" alt="groen">
+    <img src="images/vlakje_groen.svg" alt="groen">
+    <img src="images/vlakje_groen.svg" alt="groen">
+    <img src="images/vlakje_geel.svg" alt="geel">
+    <img src="images/vlakje_geel.svg" alt="geel">
+    <img src="images/vlakje_geel.svg" alt="geel">
+    <img src="images/vlakje_geel.svg" alt="geel">
+    <img src="images/vlakje_geel.svg" alt="geel">
+    <img src="images/vlakje_geel.svg" alt="geel">
+    <img src="images/vlakje_geel.svg" alt="geel">
+    <img src="images/vlakje_geel.svg" alt="geel">
+    <img src="images/vlakje_geel.svg" alt="geel">
+    <img src="images/vlakje_geel.svg" alt="geel">
+    <img src="images/vlakje_geel.svg" alt="geel">
+    <img src="images/vlakje_blauw.svg" alt="blauw">
+    <img src="images/vlakje_blauw.svg" alt="blauw">
+    <img src="images/vlakje_blauw.svg" alt="blauw">
+    <img src="images/vlakje_blauw.svg" alt="blauw">
+    <img src="images/vlakje_blauw.svg" alt="blauw">
+    <img src="images/vlakje_blauw.svg" alt="blauw">
     </div>
+
 </body>
 
 </html>
