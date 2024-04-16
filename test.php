6df0619da9fd1f5fa62d41c6503e7783eb996632
Marai <163207@student.horizoncollege.nl>
2024-04-16 12:52:25 +0200

    ja

--- test.php
+++ test.php
@@ -88,9 +88,7 @@
 
 <body>
 
-
-
-  <div class="wrapper">
+<div class="wrapper">
     <img class="ja" src="images/kip.png" data-bs-toggle="modal" data-bs-target="#exampleModal" alt="...">
     <img class="blocks" src="images/vlakje_rood.svg" alt="...">
     <img class="blocks" src="images/vlakje_rood.svg" alt="...">
@@ -113,16 +111,11 @@
     <img class="blocks" src="images/vlakje_blauw.svg" alt="...">
   </div>
 
-
-  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-    Launch demo modal
-  </button>
-
   <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">Boerderijdieren</h1>
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
           <button class="btn btn-primary" type="button" id="informatieButton">
             Informatie
           </button>
@@ -193,14 +186,17 @@
               </div>
             </div>
           </div>
-          <div class="question" style="display: none;">
-            <h1>test</h1>
-          </div>
+        </div>
+        <div class="question" style="display: none;">
+          <h1>test</h1>
         </div>
       </div>
     </div>
   </div>
+  </div>
 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+  </script>
   <script>
     document.addEventListener("DOMContentLoaded", function() {
       const informatieButton = document.getElementById('informatieButton');
@@ -261,11 +257,9 @@
         });
       });
     });
+  </script>
 
-
-    src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-    integrity = "sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL";
-    crossorigin = "anonymous"
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
   </script>
 </body>
 
