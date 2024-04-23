8f973bb0746a75507d92395708b642f155f80092
Marai de Jong <yaidenmarai@gmail.com>
2024-04-23 15:26:59 +0200

    comments

--- test.php
+++ test.php
@@ -8,10 +8,12 @@
   <style link="stylesheet" href="style.css"></style>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <style>
+    /* Styling for the Rhombuses and the modal with the cards. */
     body {
       font-family: arial;
       font-size: 16px;
       display: flex;
+
       align-items: center;
       justify-content: center;
       min-height: 100vh;
@@ -31,7 +33,7 @@
       align-items: center;
     }
 
-
+    /* This is for the positions of the rhombuses */
     .blocks {
       z-index: 1;
       grid-column: span 2;
@@ -88,6 +90,7 @@
 
 <body>
 
+  <!-- The rhombuses -->
   <div class="wrapper">
     <img class="ja" src="images/kip.png" data-bs-toggle="modal" data-bs-target="#exampleModal" alt="...">
     <img class="blocks" src="images/vlakje_rood.svg" alt="...">
@@ -110,11 +113,13 @@
     <img class="blocks" src="images/vlakje_blauw.svg" alt="...">
   </div>
 
+  <!-- Pop up modal -->
   <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
         <div class="modal-header">
           <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+          <!-- Buttons to switch from information to game view -->
           <button class="btn btn-primary" type="button" id="informatieButton">
             Informatie
           </button>
@@ -124,6 +129,7 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
+          <!-- This is where the cards are put in -->
           <div class="row">
             <div class="col-sm-6">
               <div class="card" style="width: 16rem;" id="cardVarken">
@@ -186,6 +192,7 @@
             </div>
           </div>
         </div>
+        <!-- Game part of the modal -->
         <div class="question" style="display: none;">
           <h1>test</h1>
         </div>
@@ -193,7 +200,7 @@
     </div>
   </div>
   </div>
-
+<!-- Javascript for changing the modal from information to game view, and to show and hide the text from the cards -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
   </script>
   <script>
