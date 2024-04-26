b82038d877fd77b2de0cbc52f74ff43167cf0377
Marai <163207@student.horizoncollege.nl>
2024-04-26 14:01:10 +0200

    test

--- test.php
+++ test.php
@@ -239,7 +239,7 @@
   </div>
   </div>
 
-  
+
 <!-- Javascript for changing the modal from information to game view, and to show and hide the text from the cards -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
   </script>
