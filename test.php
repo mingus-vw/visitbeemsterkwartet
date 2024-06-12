7511f4ae7926a1fd798894c94eae1e5487f097eb
Marai <163207@student.horizoncollege.nl>
2024-06-12 14:29:30 +0200

    andere styling

--- test.php
+++ test.php
@@ -13,8 +13,7 @@
 
   <!-- The rhombuses -->
   <div class="wrapper">
-    <img src="images/kaas.jpg" data-bs-toggle="modal" data-bs-target="#streekproducten" alt="Streekproducten">
-    <img src="images/logo.svg">
+    <img src="images/vlakje_rood.svg" alt="logo">
     <img src="images/vlakje_rood.svg" alt="rood">
     <img src="images/vlakje_rood.svg" alt="rood">
     <img src="images/vlakje_rood.svg" alt="rood">
@@ -26,31 +25,31 @@
     <img src="images/vlakje_rood.svg" alt="rood">
     <img src="images/vlakje_rood.svg" alt="rood">
     <img src="images/vlakje_groen.svg" alt="groen">
-    <img src="images/droogmakerij.jpg" data-bs-toggle="modal" data-bs-target="#wereld_erfgoed" alt="...">
+    <img src="images/vlakje_groen.svg"  alt="groen">
+    <img src="images/icon.svg">
     <img src="images/vlakje_groen.svg" alt="groen">
+    <img src="images/vlakje_groen.svg">
     <img src="images/vlakje_groen.svg" alt="groen">
-    <img src="images/kosterij.jpg" data-bs-toggle="modal" data-bs-target="#bijzondere_gebouwen" alt="...">
-    <img src="images/vlakje_groen.svg" alt="groen">
-    <img src="images/vlakje_groen.svg" alt="groen">
-    <img src="images/vlakje_groen.svg" alt="groen">
-    <img src="images/vlakje_groen.svg" alt="groen">
-    <img src="images/vlakje_groen.svg" alt="groen">
-    <img src="images/vlakje_groen.svg" alt="groen">
-    <img src="images/vlakje_geel.svg" alt="geel">
-    <img src="images/vlakje_geel.svg" alt="geel">
-    <img src="images/vlakje_geel.svg" alt="geel">
-    <img src="images/vlakje_geel.svg" alt="geel">
-    <img src="images/vlakje_geel.svg" alt="geel">
-    <img src="images/vlakje_geel.svg" alt="geel">
-    <img src="images/vlakje_geel.svg" alt="geel">
-    <img src="images/vlakje_geel.svg" alt="geel">
-    <img src="images/vlakje_geel.svg" alt="geel">
-    <img src="images/vlakje_geel.svg" alt="geel">
+    <img src="images/boerderijen.svg">
+    <img src="images/dorpen.svg" data-bs-toggle="modal" data-bs-target="#streekproducten">
+    <img src="images/dieren.svg">
+    <img src="images/gebouwen.svg">
+    <img src="images/vlakje_groen.svg">
+    <img src="images/kerken.svg">
+    <img src="images/streekproducten.svg">
+    <img src="images/tuinen.svg">
+    <img src="images/waterbeheer.svg">
+    <img src="images/wegen_dijken_bruggen.svg">
+    <img src="images/vlakje_geel.svg">
+    <img src="images/werelderfgoed.svg">
+    <img src="images/hollandse_waterlinies.svg">
+    <img src="images/historische_figuren.svg">
+    <img src="images/beemsterkleuren.svg">
     <img src="images/vlakje_geel.svg" alt="geel">
     <img src="images/vlakje_blauw.svg" alt="blauw">
+    <img src="images/beemster_vanzelfsprekend.svg">
     <img src="images/vlakje_blauw.svg" alt="blauw">
-    <img src="images/vlakje_blauw.svg" alt="blauw">
-    <img src="images/vlakje_blauw.svg" alt="blauw">
+    <img src="images/visit_beemster.svg">
     <img src="images/vlakje_blauw.svg" alt="blauw">
     <img src="images/vlakje_blauw.svg" alt="blauw">
     <img src="images/vlakje_blauw.svg" alt="blauw">
