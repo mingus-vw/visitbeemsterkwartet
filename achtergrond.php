cc6b10dc60d4d87a3954e511c430e5712d320610
Marai de Jong <yaidenmarai@gmail.com>
2024-06-16 11:56:57 +0200

    ja

--- achtergrond.php
+++ achtergrond.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Beemster Kwartet</title>
+  <link rel="stylesheet" href="test.css"> <!-- Zorg ervoor dat je stylesheet correct wordt gekoppeld -->
 </head>
 <body>
-<img src="images/Homepage_desktop.png" alt="">
+  <div class="background-image"></div>
 </body>
 </html>
